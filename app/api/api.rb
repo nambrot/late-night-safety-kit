df2b1ef1121910cef32f6b81edcaf0a73ef6d007
Guy Aridor <guy.aridor@nutonian.com>
2014-08-09 15:10:45 -0400

    fix police stations retrieval

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,14 +1,14 @@
 require 'httparty'
 
 class Api
-
+    mbta_api_key ="wX9NwuHnZU2ToO7GmGR9uw"
     def crimes()
         source = "https://data.cityofboston.gov/resource/7cdf-6fgx.json"
         resp = Net::HTTP.get_response(URI.parse(source))
         data = resp.body
         crimes = Array.new
         crime = Struct.new(:latitude, :longitude, :incident, :dayofweek, :time, :day)
-        result = JSON.parse(data).each do |item|
+        JSON.parse(data).each do |item|
             crimes.push(crime.new(item['location']['latitude'], item['location']['longitude'], item['incident_type_description'], item['day_week'], item['fromdate'].split("T")[1], item["fromdate"].split("T")[0]))
         end
         return crimes
@@ -19,11 +19,17 @@ class Api
         resp = Net::HTTP.get_response(URI.parse(source))
         data = resp.body
         police_stations = Array.new
-        station = Struc.new(:latitude, :longitude, :address)
-        result = JSON.parse(data).each do |item|
-            police_stations.push(station.new(item['location']['latitude'], item['location']['longitude'], item['human_address']['address']))
+        station = Struct.new(:latitude, :longitude, :address)
+        JSON.parse(data).each do |item|
+            human_address = JSON.parse(item["location"]["human_address"])
+            police_stations.push(station.new(item['location']['latitude'], item['location']['longitude'], human_address["address"]))
         end
         return police_stations
     end
+
+    def mbta()
+        url = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?"
+    end
+
 
 end
