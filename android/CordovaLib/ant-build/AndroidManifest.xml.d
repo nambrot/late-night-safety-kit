f1d620a6b92a5f42f79e3880977c92dde0985272
connormathews <connor.mathews@constanttherapy.com>
2014-08-09 17:05:17 -0400

    Android cordova files.

---
+++ android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/connormathews/late-night-safety-kit/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/connormathews/late-night-safety-kit/android/CordovaLib/AndroidManifest.xml \
---
+++ android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes.dex : \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/BuildConfig.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/LNSK.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/R$attr.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/R$drawable.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/R$string.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/R$xml.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/classes/com/latenightsafetykit/android/R.class \
+/Users/connormathews/late-night-safety-kit/android/ant-build/dexedLibs/classes-2a6e85aa82d03fe6ab7bb86616955076.jar \
