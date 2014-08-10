e9d29baabc32bf36548b2467b8ef8609cdad74bd
Nam Chu Hoai <nambrot@googlemail.com>
2014-08-10 13:01:51 -0400

    fix timer

--- config/clock.rb
+++ config/clock.rb
@@ -3,5 +3,5 @@ include Clockwork
 require File.expand_path('../../config/boot', __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
  
-every(1.minutes, 'check.timers') { Timer.check_timers }
+every(10.seconds, 'check.timers') { Timer.check_timers }
  
