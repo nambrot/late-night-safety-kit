de9b9027314bb1165b558d1134957d6959d6c616
Guy Aridor <guy.aridor@nutonian.com>
2014-08-09 21:34:48 -0400

    add show method to timer class

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -11,11 +11,20 @@ class TimersController < ApplicationController
             if DateTime.now.to_i() > user[:expires_at].to_i()
                 api = Api.new
                 JSON.parse(user[:numbers]).each do |contact|
-                    api.sendtext(contact['number'], "Help I'm in trouble!")
+                    if contact['number']
+                        api.sendtext(contact['number'], "Help I'm in trouble!")
+                    elsif contact['email']
+                        api.sendemail(contact['email'], "help", "Help I'm in trouble!")
+                    end
                 end
             end
         end
         render :plain => "OK"
+    end
+
+    def show()
+        user = Timer.find_by(user_id: params[:user_id])
+        render :plain => user
     end
 
     def destroy()
